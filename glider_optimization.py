42548be895285931df8ec8cb52c04958457573c1
mathesonprice <matheson.price@gmail.com>
2021-03-30 15:04:10 -0600

    Cleaned up Glider Optimization

--- glider_optimization.py
+++ glider_optimization.py
@@ -3,6 +3,9 @@ import numpy as np
 import json
 from scipy.optimize import minimize
 
+#TODO: trim at launch velocity
+
+
 def trim_func(x,Cl,static_margin, Cn_beta, Cl_beta):
     with open('glider_2.0.json') as f:
         airplane = json.load(f)
@@ -51,68 +54,6 @@ def trim(Cl,static_margin, Cn_beta, Cl_beta):
     args = (Cl,static_margin, Cn_beta, Cl_beta)
     res = minimize(trim_func, x, constraints = constraints,args = args)
     print(res)
-
-def area2412(chord):
-    # Calculates the area of the NACA2412 airfoil given a chord length
-    x = np.arange(0,1.00001,0.00001)
-    p = 0.4
-    m = 0.02
-    yc1 = m/p**2*(2*p*x - x**2)*np.where(x<=p,x,0)
-    yc2 = m/(1-p)**2*(1-2*p + 2*p*x - x**2)*np.where(x>p,x,0)
-    yc = yc1 + yc2
-    dyc1 = 2*m/p**2*(p-x)*np.where(x<=p,x,0)
-    dyc2 = 2*m/(1-p)**2*(p-x)*np.where(x>p,x,0)
-    dyc = dyc1+dyc2
-    yt = 0.12/0.2*(0.2969*x**0.5 - 0.126*x - 0.3516*x**2 + 0.2843*x**3 -0.1015*x**4)
-    theta = np.arctan(dyc)
-    xu = x - yt*np.sin(theta)
-    yu = yc + yt*np.cos(theta)
-    xl = x + yt*np.sin(theta)
-    yl = yc - yt*np.cos(theta)
-    area1 = np.trapz(yu*chord,xu*chord)
-    area2 = abs(np.trapz(yl*chord,xl*chord))
-    return area1 + area2
-
-def area0012(chord):
-    # Calculates the area of the NACA0012 airfoil given a chord length
-    x = np.arange(0,1.00001,0.00001)
-    # yc1 = m/p**2*(2*p*x - x**2)*np.where(x<=p,x,0)
-    # yc2 = m/(1-p)**2*(1-2*p + 2*p*x - x**2)*np.where(x>p,x,0)
-    yc = 0
-    # dyc1 = 2*m/p**2*(p-x)*np.where(x<=p,x,0)
-    # dyc2 = 2*m/(1-p)**2*(p-x)*np.where(x>p,x,0)
-    dyc = 0
-    yt = 0.12/0.2*(0.2969*x**0.5 - 0.126*x - 0.3516*x**2 + 0.2843*x**3 -0.1015*x**4)
-    theta = np.arctan(dyc)
-    xu = x - yt*np.sin(theta)
-    yu = yc + yt*np.cos(theta)
-    xl = x + yt*np.sin(theta)
-    yl = yc - yt*np.cos(theta)
-    area1 = np.trapz(yu*chord,xu*chord)
-    area2 = abs(np.trapz(yl*chord,xl*chord))
-    return area1 + area2
-
-
-def structural(x):
-    wing_volume = area2412(x[2])*x[0]       # volume of wing
-    emp_volume = area0012(x[1])*x[0]/3      # Span of horiz and vert are 1/3 span of wing
-    foam_weight = (wing_volume + emp_volume) * 0.7
-    aspect_ratio = x[0]**2/(x[0]*x[1])
-    wf = x[4] - foam_weight
-    rhs = 16/3*243/(1+wf*x[0]/(x[2]-wf))
-    return rhs-aspect_ratio
-
-def square9(x):
-    # Check that all the foam elements are within the 9 square feet
-    wing_area = x[0]*x[1]
-    emp_area = x[0]*x[1]*3
-    return 9-(wing_area+emp_area)
-
-def weight(x):
-    wing_volume = area2412(x[1])*x[0]       # volume of wing
-    emp_volume = area0012(x[1])*x[0]/3      # Span of horiz and vert are 1/3 span of wing
-    foam_weight = (wing_volume + emp_volume) * 0.7
-    dowel_weight = (0.75/24)**2*np.pi*6*27
 
 if __name__ == '__main__':
     trim(0.5, 0.35, 0.1, -0.05)
