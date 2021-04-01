ae59f3e9e642ebbdff7df64f42c7ce6541a50410
mathesonprice <matheson.price@gmail.com>
2021-03-31 20:57:59 -0600

    Updates

--- glider_optimization.py
+++ glider_optimization.py
@@ -13,21 +13,17 @@ def trim_func(x,Cl,static_margin, Cn_beta, Cl_beta):
     with open('scene.json') as f:
         scene = json.load(f)
     f.close()
+    wing_twist = 2.5        # angle to twist wing down from mounting angle.
     airplane['CG'] = [x[0],0.0,0.0]
     airplane['wings']['main_wing']['twist'] = [[0.0, x[1],
-                                                1.0, x[1]-2.5]]
+                                                1.0, x[1]-wing_twist]]
     airplane['wings']['outside_wings']['twist'] =  [[0.0, 0,
                                                     1.0, 0]]
     airplane['wings']['H_Stab']['connect_to']['dx'] = x[2]
     airplane['wings']['V_Stab']['connect_to']['dx'] = x[3]
     airplane['wings']['main_wing']['dihedral'] = x[4]
-    weight = airplane['weight']
-    #scene['scene']['aircraft']['glider_2.0']['state']['velocity'] = np.sqrt(10/(0.5*weight/32.217))
     with open('glider_2.0.json', 'w') as fp:
         json.dump(airplane,fp,indent = 4)
-    fp.close()
-    with open('scene.json', 'w') as fp:
-        json.dump(scene,fp,indent = 4)
     fp.close()
     my_scene = MX.Scene('scene.json')
     FM_results = my_scene.solve_forces(non_dimensional = True)
@@ -62,7 +58,11 @@ def trim(static_margin, Cn_beta, Cl_beta):
     print(res)
 
 if __name__ == '__main__':
-    trim( 0.35, 0.1, -0.05)
+    trim(0.35, 0.1, -0.05)
+    import req_check
+    req_check.check_requirements('glider_2.0.json')
+    scene = MX.Scene('scene.json')
+    scene.export_dxf()
     # To use, define everything in the JSON file, then define your trim(static_margin, Cn_beta, Cl_beta)
     # Pressing run will update the CGx, H and V Stab location and dihedral inside of the JSON file.
 
